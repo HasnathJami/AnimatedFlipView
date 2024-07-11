dc20443f23b221ece0c533865d842b4a92d4c0b3
hasnath.chowdhury <hasnath.chowdhury@reddotdigitalit.com>
2024-07-11 11:37:50 +0600

    init library

---
+++ app/src/main/java/com/example/animatedflipviewhost/AnimatedFlipViewHostActivity.kt
@@ -0,0 +1,13 @@
+package com.example.animatedflipviewhost
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.animated_flip_view.AnimatedFlipView
+
+class AnimatedFlipViewHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animated_flip_view_host)
+    }
+}
