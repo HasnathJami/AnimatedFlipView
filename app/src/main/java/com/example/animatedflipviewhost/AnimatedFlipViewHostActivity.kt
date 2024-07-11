1d56564534749b0f7ab0bcfc91dcbe269ca1ebda
hasnath.chowdhury <hasnath.chowdhury@reddotdigitalit.com>
2024-07-11 12:35:06 +0600

    library related issue fix

--- app/src/main/java/com/example/animatedflipviewhost/AnimatedFlipViewHostActivity.kt
+++ app/src/main/java/com/example/animatedflipviewhost/AnimatedFlipViewHostActivity.kt
@@ -7,5 +7,7 @@ class AnimatedFlipViewHostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animated_flip_view_host)
+
+//        AnimatedFlipView.showToast(this@AnimatedFlipViewHostActivity, "Test AFV")
     }
 }
