6e935a47ad32b65a3cec6e6f49f2817364799d33
hasnath.chowdhury <hasnath.chowdhury@reddotdigitalit.com>
2024-07-11 21:21:16 +0600

    flip view implementation //wip-unstable

--- app/src/main/java/com/example/animatedflipviewhost/AnimatedFlipViewHostActivity.kt
+++ app/src/main/java/com/example/animatedflipviewhost/AnimatedFlipViewHostActivity.kt
@@ -1,7 +1,9 @@
 package com.example.animatedflipviewhost
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.example.animated_flip_view.AnimatedFlipView
 
 class AnimatedFlipViewHostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,20 @@ class AnimatedFlipViewHostActivity : AppCompatActivity() {
         setContentView(R.layout.activity_animated_flip_view_host)
 
 //        AnimatedFlipView.showToast(this@AnimatedFlipViewHostActivity, "Test AFV")
+        val animatedFlipView = findViewById<AnimatedFlipView>(R.id.myEasyFlipView)
+
+//        animatedFlipView.flip()
+        animatedFlipView.flipToFront()
+//        animatedFlipView.findViewById<LinearLayout>(R.id.frontViewContent).setOnClickListener {
+//            animatedFlipView.flipToBack()
+//           // animatedFlipView.flip()
+//        }
+
+//// Flip to back
+//        animatedFlipView.flipToBack()
+//
+//// Flip to front
+//        animatedFlipView.flipToFront()
+
     }
 }
